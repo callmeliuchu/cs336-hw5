3a44361e63804163ce924b7c3da84cbf2715d2b6
liuchu <1371772034@qq.com>
2025-09-06 12:44:53 +0800

    update

--- sft.py
+++ sft.py
@@ -156,8 +156,8 @@ def sft_experiment():
         # Move input data to the same device as the model
         input_ids = res['input_ids'].to('cuda:0')
         labels = res['labels'].to('cuda:0')
+        response_mask = res['response_mask'].to('cuda:0')
         policy_log_probs = get_response_log_probs(model, input_ids, labels)['log_probs']
-        response_mask = res['response_mask']
         optimizer.zero_grad()
         loss, _ = sft_microbatch_train_step(policy_log_probs, response_mask, gradient_accumulation_steps=1, normalize_constant=1.0)
         optimizer.step()
